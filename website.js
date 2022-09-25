1647f6a849591c54fe372f02cea3beb5ce564bfb
Basilios <vasyl.tyshchuk.xyz@gmail.com>
2022-09-25 17:08:29 +0300

    Update general scraper

--- website.js
+++ website.js
@@ -1,25 +1,29 @@
-async function website(value, tp, html) {
+async function website(value, tp, doc) {
     let url = await tp.system.clipboard()
 
-    if (html === undefined) {
-        let page = await tp.obsidian.request({url})
+    if (doc === undefined) {
+        let page = await tp.obsidian.request(url)
         let p = new DOMParser()
-        html = p.parseFromString(page, "text/html")
+        doc = p.parseFromString(page, "text/html")
     }
 
     // Alias for querySelector
-    let $ = s => html.querySelector(s)
+    let $ = s => doc.querySelector(s)
 
     switch(value) {
         case "url":
-            return url
+            return url.trim()
             break
         case "title":
-            return $("title")?.textContent.trim() || $("meta[property='title']")?.content
+            return $("meta[property='title']")?.content || $("meta[property='og:title']")?.content || $("meta[name='twitter:title']")?.content || $("title")?.textContent.trim() || ""
             break
         case "description":
-            let description = $("meta[name='description']")?.content || $("meta[property='og:description']")?.content || ""
-            return description.replace(/&amp;/g, "&").replace(/&#039;/g, "'").replace(/&quot;/g, '"')
+            let description = $("meta[property='og:description']")?.content || $("meta[name='description']")?.content || $("meta[name='twitter:description']")?.content || ""
+            description = description.replace(/&#039;/g, "'").replace(/&#39;/g, "'").trim()
+            return description.replace(/&amp;/g, "&").replace(/&quot;/g, '"').replace(/&nbsp;/g, " ")
+            break
+        case "image":
+            return $("meta[property='og:image']")?.content || $("meta[name='twitter:image']")?.content || $("meta[name='twitter:image:src']")?.content || ""
             break
         default:
             new Notice("Incorrect parameter: " + value, 5000)
