623f5a0814487e9f5682ce78c8130c8a4a6b2466
Basilios <vasyl.tyshchuk.xyz@gmail.com>
2023-05-02 10:42:53 +0300

    Try to parse application/json from Goodreads
    It isn't consistent and difficult to parse

--- goodreads.js
+++ goodreads.js
@@ -5,33 +5,42 @@ async function goodreads(value, tp, doc) {
     let p = new DOMParser();
     doc = p.parseFromString(page, "text/html");
   }
+
+  let json = JSON.parse(
+    doc.querySelector("script[type='application/json']").innerHTML
+  );
+
+  json = json.props.pageProps.apolloState;
+  let bookId = Object.keys(json).find(key => key.match(/^Book:/));
+  let workId = Object.keys(json).find(key => key.match(/^Work:/));
+
   // Alias for querySelector
   let $ = (s) => doc.querySelector(s);
 
+  // TODO: authors, genres
   switch (value) {
     case "url":
-      return $("link[rel='canonical']").href;
+      return json[bookId]?.webUrl ?? "";
     case "title":
-      let title = $("h1[id='bookTitle']").innerHTML ?? "";
-      return title.trim().replace(/&amp;/g, "&");
+      return json[bookId]?.title ?? "";
     case "authors":
-      return authors(doc);
+      return authors(json);
     case "authorsQ":
       // Quotes
-      let authorsQ = authors(doc);
+      let authorsQ = authors(json);
       return '"' + authorsQ.replace(/, /g, '", "') + '"';
     case "authorsL":
       // List
-      let authorsL = authors(doc);
+      let authorsL = authors(json);
       return "\n- " + authorsL.replace(/, /g, "\n- ");
     case "authorsW":
       // Wiki links
-      let authorsW = authors(doc);
+      let authorsW = authors(json);
       return "[[" + authorsW.replace(/, /g, "]], [[") + "]]";
     case "isbn":
-      return $("meta[property='books:isbn']").content ?? "";
+      return json[bookId]?.details?.isbn13 ?? "";
     case "published":
-      return published(doc);
+      return getPubDate(doc) ?? "";
     case "genres":
       return genres(doc);
     case "genresQ":
@@ -44,50 +53,51 @@ async function goodreads(value, tp, doc) {
       let genresW = genres(doc);
       return "[[" + genresW.replace(/, /g, "]], [[") + "]]";
     case "cover":
-      return $("img[id='coverImage']").src ?? "";
+      return json[bookId]?.imageUrl ?? "";
     case "pageCount":
-      return $("meta[property*='page_count']").content ?? "";
+      return json[bookId]?.details?.numPages ?? "";
     case "description":
-      return $("meta[property='og:description']").content.trim() ?? "";
+      return $("meta[property='og:description']")?.content.trim() ?? "";
+    case "descriptionFull":
+      let desc = json[bookId]?.description ?? "";
+      // Strip html
+      desc = desc.replace(/<[^>]*>?/gm, '');
+      return desc;
     case "rating":
-      return $("span[itemprop='ratingValue']").innerText.trim() ?? "";
+      return json[workId]?.stats?.averageRating ?? "";
     default:
       new Notice("Incorrect parameter: " + value, 5000);
   }
 }
 
-function authors(doc) {
-  let authors = doc.querySelectorAll("span[itemprop=name]");
-  // Create an array. Replace multiple spaces with single space
-  authors = Array.from(authors, (authors) =>
-    authors.textContent.replace(/ +(?= )/g, "")
-  );
-  return authors.join(", ");
+function authors(json) {
+  let authors = "";
+  if (json.author != null) {
+    authors = json.author.map((author) => author.name);
+    authors = JSON.stringify(authors)
+      .replace(/null,?/g, "")
+      .replace(/","/g, ", ")
+      .replace(/\["/g, "")
+      .replace(/\"]/, "");
+  }
+  return authors;
 }
 
-function published(doc) {
-  var pubDate = "";
-  let nobr = doc.querySelector("nobr[class='greyText']");
-  if (nobr) {
-    pubDate = nobr.innerHTML;
-  } else {
-    let details = doc.querySelectorAll("div[id='details'] div.row");
-    pubDate = details[details.length - 1].innerHTML;
+function getPubDate(doc) {
+  let publicationInfo = doc.querySelector('p[data-testid="publicationInfo"]');
+  if (!publicationInfo) {
+    return '';
   }
-  const regex = /\d{4}/;
-  let match = regex.exec(pubDate);
-  if (match) {
-    return match[0];
-  }
+  let dateString = publicationInfo.innerHTML.match(/First published\s(.*)/);
+  return dateString ? dateString[1] : '';
 }
 
 function genres(doc) {
-  let genres = doc.querySelectorAll(
-    "a[class='actionLinkLite bookPageGenreLink']"
-  );
-  genres = Array.from(genres, (genres) => genres.textContent.toLowerCase());
-  genres = [...new Set(genres)]; // Remove duplicates
-  return genres.join(", ");
+  let genreElements = doc.querySelectorAll('.BookPageMetadataSection__genreButton .Button__labelItem');
+  if (!genreElements || genreElements.length === 0) return "";
+
+  let genres = Array.from(genreElements, (el) => el.textContent);
+  return [...new Set(genres)].join(", ");
 }
 
 module.exports = goodreads;
